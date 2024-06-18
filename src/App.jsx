ed2713877056c645fbf1e78e6ada5260c3920b0d
ZL <ray040406@gmail.com>
2024-06-19 01:03:37 +1200

    reuse components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <Hello />
+      <Hello />
+      <Hello />
     </div>
   );
 }
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,5 +1,15 @@
+const name = "ZLRKW11";
+
+function displayMessage() {
+  return "Wow!";
+}
+
 function Hello() {
-  return <h1>Hello from a component</h1>;
+  return (
+    <h1>
+      Hello from a component! {displayMessage()} {name}
+    </h1>
+  );
 }
 
 export default Hello;
