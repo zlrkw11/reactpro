17cfeee95fc6e15be1aba080c623fad7187ceac3
ZL <ray040406@gmail.com>
2024-06-18 22:26:57 +1200

    put on

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Hello from "./components/Hello";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
 }
 
 export default App;
