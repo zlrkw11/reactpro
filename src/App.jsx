58446c72a91024ed8ec104495878601c5c6743b1
ZL <ray040406@gmail.com>
2024-06-18 22:08:34 +1200

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return <div className="App">Hello World!</div>;
 }
 
 export default App;
