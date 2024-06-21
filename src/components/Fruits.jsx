f7caad0b5096df6d71dd55ec56a609ebe06350c8
ZL <ray040406@gmail.com>
2024-06-21 23:19:53 +1200

    array of fruit objects

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,5 +1,32 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
+  // const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
+  const fruits = [
+    {
+      name: "Apple",
+      price: 10,
+      sweetness: "medium",
+    },
+    {
+      name: "Mango",
+      price: 25,
+      sweetness: "high",
+    },
+    {
+      name: "Banana",
+      price: 5,
+      sweetness: "high",
+    },
+    {
+      name: "Orange",
+      price: 15,
+      sweetness: "medium",
+    },
+    {
+      name: "Pineapple",
+      price: 15,
+      sweetness: "low",
+    },
+  ];
   return (
     <div>
       {fruits.map((fruit) => (
