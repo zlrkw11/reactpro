65819d532ad2a7034964dda33e9038767ff325ce
ZL <ray040406@gmail.com>
2024-06-21 21:25:04 +1200

    pass an object instead

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import Hello from "./components/Hello";
 
 function App() {
   const seatNumbers = [1, 4, 7];
+  const person = {
+    name: "Ray",
+    message: "Hi",
+    food: "noodles",
+    seatNumbers: [1, 1, 1],
+  };
+
   return (
     <div className="App">
-      <Hello name="ZL" message="Wind" emoji="😎" seatNumber={seatNumbers} />
+      <Hello person={person} />
     </div>
   );
 }
