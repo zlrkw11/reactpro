8f965880da87d0b560fa386f0213d8001d134eee
ZL <ray040406@gmail.com>
2024-06-21 21:20:49 +1200

    pass array as a prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Hello from "./components/Hello";
 
 function App() {
+  const seatNumbers = [1, 4, 7];
   return (
     <div className="App">
-      <Hello name="ZL" message="Wind" emoji="😎" />
+      <Hello name="ZL" message="Wind" emoji="😎" seatNumber={seatNumbers} />
     </div>
   );
 }
