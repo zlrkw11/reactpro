339e4ed5458f10016f91e8d9559e4a5977bdb069
ZL <ray040406@gmail.com>
2024-06-21 21:47:50 +1200

    mapped fruit to display every name

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,4 +1,10 @@
 export default function Fruits() {
   const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
-  return <div>{fruits}</div>;
+  return (
+    <div>
+      {fruits.map((fruit) => (
+        <h1>{fruit}</h1>
+      ))}
+    </div>
+  );
 }
