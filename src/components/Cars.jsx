e65597bc78e07d48594c2e3c503c641ec45bac67
ZL <ray040406@gmail.com>
2024-06-22 23:35:39 +1200

    conditional rendering

---
+++ src/components/Cars.jsx
@@ -0,0 +1,16 @@
+export default function Cars() {
+  const display = false;
+  if (display) {
+    return (
+      <div>
+        <h3>Mazda</h3>;
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h3>Honda</h3>
+      </div>
+    );
+  }
+}
