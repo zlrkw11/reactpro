e36b54c829265deb8147758525f422671a05400c
ZL <ray040406@gmail.com>
2024-06-24 01:06:47 +1200

    ternary op jsx

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -2,14 +2,8 @@ import Civic from "./Civic";
 import Lancer from "./Lancer";
 
 export default function Cars() {
-  let Car1 = <h1>Car1</h1>;
-  let Car2 = <h1>Car2</h1>;
-  let Car;
-  const display = true;
-  if (display) {
-    Car = Car1;
-  } else {
-    Car = Car2;
+  let display = false;
+  {
+    return display ? <Civic /> : <Lancer />;
   }
-  return Car;
 }
