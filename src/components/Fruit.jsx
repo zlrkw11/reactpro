b7c78f70aeb77ef296a4ab5ae9ec3bbdc0df0aff
ZL <ray040406@gmail.com>
2024-06-24 01:12:19 +1200

    conditionally renders each fruit component

--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -2,6 +2,13 @@ export default function Fruit({ name, price, sweetness }) {
   return (
     <li>
       {/* The fruit is {name} and it's ${price}, with {sweetness} sugar amount */}
+      {price > 10 ? (
+        <h3>
+          The fruit is {name} and it's ${price}, with {sweetness} sugar amount
+        </h3>
+      ) : (
+        ""
+      )}
     </li>
   );
 }
