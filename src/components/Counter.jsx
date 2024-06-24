a0e2add13623b8b95d52d1ced8356380002acf51
ZL <ray040406@gmail.com>
2024-06-24 23:19:02 +1200

    handleClick functions

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState;
+  const [count, setCount] = useState(0);
+  function handleClickPlus() {
+    setCount(count + 1);
+  }
+
+  function handleClickMinus() {
+    setCount(count - 1);
+  }
+
   return (
     <div>
-      <h1>Count value: </h1>
+      <h1>Count value: {count}</h1>
+
+      <button onClick={handleClickPlus}>Increment</button>
+      <button onClick={handleClickMinus}>Decrement</button>
     </div>
   );
 }
