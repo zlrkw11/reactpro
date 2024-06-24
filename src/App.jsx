c9d7d20e38c620f38026366e947ae183740d8bbc
ZL <ray040406@gmail.com>
2024-06-24 23:02:50 +1200

    set up state file

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Hello from "./components/Hello";
 import Fruits from "./components/Fruits";
 import Cars from "./components/Cars";
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
     </div>
   );
 }
