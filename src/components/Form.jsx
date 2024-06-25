42cabde6d44d88249df56b92ec32b0e20a83e2f0
ZL <ray040406@gmail.com>
2024-06-26 01:43:18 +1200

    add states

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [name, setName] = useState("");
   function handleChange() {
     console.log("Change occured");
   }
+
   return (
     <div>
       <form>
-        <input onChange={handleChange} type="text" value="Name"></input>
+        <input onChange={handleChange} type="text" value={name}></input>
       </form>
     </div>
   );
