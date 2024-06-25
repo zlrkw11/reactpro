a528b36ad0ac7879f9ad4c6a5dd90bceed7e3acb
ZL <ray040406@gmail.com>
2024-06-26 01:28:05 +1200

    incrementBy state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,20 +2,29 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-  function handleClickPlus() {
-    setCount(count + 1);
+  const [incrementBy, setIncrementBy] = useState(1);
+
+  function increment() {
+    setCount(count + incrementBy);
   }
 
-  function handleClickMinus() {
-    setCount(count - 1);
+  function decrement() {
+    setCount(count - incrementBy);
+  }
+
+  function increaseIncrement() {
+    setIncrementBy(incrementBy + 1);
   }
 
   return (
     <div>
       <h1>Count value: {count}</h1>
 
-      <button onClick={handleClickPlus}>Increment</button>
-      <button onClick={handleClickMinus}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+
+      <h1>We are incrementing / decrementing the value by: {incrementBy}</h1>
+      <button onClick={increaseIncrement}>Increase Increment</button>
     </div>
   );
 }
