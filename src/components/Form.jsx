c1ed96accdca8c4fe0b2d259de452c157995b38a
ZL <ray040406@gmail.com>
2024-06-26 15:28:39 +1200

    last name input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState("");
-  function handleChange(e) {
-    setName(e.target.value);
-  }
+  const [name, setName] = useState({ firstName: "", lastName: "" });
 
   return (
     <div>
       <form>
         <input
-          // onChange={function demo(e) {
-          //   return handleChange(e);
-          // }}
           onChange={(e) => {
-            handleChange(e);
+            setName({ firstName: e.target.value });
           }}
           type="text"
-          value={name}
+          value={name.firstName}
+        ></input>
+
+        <input
+          onChange={(e) => {
+            setName({ firstName: e.target.value });
+          }}
+          type="text"
+          value={name.firstName}
         ></input>
       </form>
     </div>
