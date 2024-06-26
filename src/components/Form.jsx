c59dc0ad9b923f37498271e035b1e760a2a9610a
ZL <ray040406@gmail.com>
2024-06-26 12:09:31 +1200

    change name

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,15 +3,18 @@ import { useState } from "react";
 export default function Form() {
   const [name, setName] = useState("");
   function handleChange(e) {
-    console.log(e.target.value);
+    setName(e.target.value);
   }
 
   return (
     <div>
       <form>
         <input
-          onChange={function demo(e) {
-            return handleChange(e);
+          // onChange={function demo(e) {
+          //   return handleChange(e);
+          // }}
+          onChange={(e) => {
+            handleChange(e);
           }}
           type="text"
           value={name}
